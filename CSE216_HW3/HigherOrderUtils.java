a25315571a579704d92a764f3fa674ffa0f37c67
Brian Chau <brianchau0729@gmail.com>
2025-04-19 15:09:25 -0400

    Did questions 3 and 4

--- CSE216_HW3/HigherOrderUtils.java
+++ CSE216_HW3/HigherOrderUtils.java
@@ -1,5 +1,5 @@
 import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.*;
 
 public class HigherOrderUtils {
     static interface NamedBiFunction<T, U, R> extends BiFunction<T, U, R> {
@@ -39,7 +39,7 @@ public class HigherOrderUtils {
 
         @Override
         public Double apply(Double o, Double o2) {
-            return o + o2;
+            return o * o2;
         }
     };
 
@@ -51,7 +51,34 @@ public class HigherOrderUtils {
 
         @Override
         public Double apply(Double o, Double o2) {
-            return o + o2;
+            if(o2 == 0){
+                throw new IllegalArgumentException("Can't divide by zero");
+            }
+            return o / o2;
         }
     };
+
+    public static <T,S extends BiFunction<T,T,T>> T zip(List<T> args, List<S> bifunctions){
+        for(int i = 0; i < bifunctions.size(); i++){
+            S function = bifunctions.get(i);
+            T arg1 = args.get(i);
+            T args2 = args.get(i+1);
+            args.set(i+1, function.apply(arg1, args2));
+            System.out.println(args.toString());
+        }
+        return args.get(args.size() -1);
+    }
+
+    public static void main(String... args) {
+        List<Double> numbers = Arrays.asList(-0.5, 2d, 3d, 0d, 4d); // documentation example
+        List<NamedBiFunction<Double, Double, Double>> operations = Arrays.asList(add,multiply,add,divide);
+        Double d = zip(numbers, operations); // expected correct value: 1.125
+        System.out.println(d);
+        // different use case, not with NamedBiFunction objects
+        List<String> strings = Arrays.asList("a", "n", "t");
+        // note the syntax of this lambda expression
+        BiFunction<String, String, String> concat = (s, t) -> s + t;
+        String s = zip(strings, Arrays.asList(concat, concat)); // expected correct value: "ant"
+        System.out.println(s);
+    }
 }
